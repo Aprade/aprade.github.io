475db9ca2c497388b4c497e0a235d366006fdd73
Joao Koritar <jakoritarleite@gmail.com>
2020-11-16 21:37:10 -0300

    add react-router-dom usage, Construction and NotFound page as Routes, and add Manrope as main font

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import './App.css';
+import './assets/fonts/manrope/index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import { Construction, NotFound } from './pages';
+import { Footer } from './components';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={ Construction }></Route>
+				<Route component={ NotFound } />
+			</Switch>
+			<Footer />
+		</BrowserRouter>
+	);
 }
 
 export default App;
